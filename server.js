8aebef5c0789012e3824b336a61cdf51b8c101fa
KAWA <dergilrku86@gmail.com>
2026-06-02 16:42:21 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+app.use(cors());
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.login(process.env.TOKEN);
+
+app.get("/members", async (req, res) => {
+  const guild = client.guilds.cache.get("1498763376598319116");
+
+  if (!guild) {
+    return res.json({ error: "Guild not found" });
+  }
+
+  await guild.members.fetch();
+
+  const members = guild.members.cache.map(member => ({
+    name: member.user.username,
+    avatar: member.user.displayAvatarURL(),
+    roles: member.roles.cache
+      .filter(r => r.name !== "@everyone")
+      .map(r => r.name)
+  }));
+
+  res.json(members);
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+app.listen(process.env.PORT || 3000);
