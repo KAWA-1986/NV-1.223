77b152801419a6c88b7c65b8cf3527cee8799bd5
KAWA <dergilrku86@gmail.com>
2026-06-02 17:30:33 +0300

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const { Client, GatewayIntentBits } = require("discord.js");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 const client = new Client({
