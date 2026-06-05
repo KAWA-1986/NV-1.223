d3d7ab8c8744592619318bef44248e84705be77f
KAWA <dergilrku86@gmail.com>
2026-06-05 10:19:55 +0300

    server.js

--- server.js
+++ server.js
@@ -11,13 +11,15 @@ app.use(cors());
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences
   ]
 });
 
 client.login(process.env.TOKEN);
 
 app.get("/members", async (req, res) => {
+
   const guild = client.guilds.cache.get("1498763376598319116");
 
   if (!guild) {
@@ -35,10 +37,50 @@ app.get("/members", async (req, res) => {
   }));
 
   res.json(members);
+
+});
+
+app.get("/activity", async (req, res) => {
+
+  const guild = client.guilds.cache.get("1498763376598319116");
+
+  if (!guild) {
+    return res.json([]);
+  }
+
+  await guild.members.fetch();
+
+  const data = guild.members.cache.map(member => {
+
+    const presence = member.presence;
+
+    let activity = "Немає активності";
+
+    if (
+      presence &&
+      presence.activities &&
+      presence.activities.length > 0
+    ) {
+      activity = presence.activities[0].name;
+    }
+
+    return {
+      name: member.user.username,
+      avatar: member.user.displayAvatarURL(),
+      status: presence?.status || "offline",
+      activity: activity
+    };
+
+  });
+
+  res.json(data);
+
 });
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
